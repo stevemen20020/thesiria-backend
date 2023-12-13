a0752b2ef950240afc1b49cf5b0e76f6b7bf68dd
stevemen20020 <david@alphadev.io>
2023-12-12 19:10:32 -0600

    Se agrego el controlador de elemento

--- src/classes/ModelFactory.js
+++ src/classes/ModelFactory.js
@@ -1,5 +1,6 @@
 const {PlayableCharacter} = require("../models/PlayableCharacter.js")
 const {Affinity} = require("../models/Affinity.js")
+const {Element} = require("../models/Element.js")
 
 class ModelFactory {
     static create(model, payload) {
@@ -8,6 +9,8 @@ class ModelFactory {
                 return new PlayableCharacter(payload)
             case 'affinity':
                 return new Affinity(payload)
+            case 'element':
+                return new Element(payload)
             default:
                 return 0
         }
