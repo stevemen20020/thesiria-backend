89cadf5f053f40da30870faf707e95ccf6813da4
stevemen20020 <david@alphadev.io>
2023-12-23 12:24:44 -0600

    Se agrego modelo de skill y de user

--- src/classes/ModelFactory.js
+++ src/classes/ModelFactory.js
@@ -1,6 +1,8 @@
 const {PlayableCharacter} = require("../models/PlayableCharacter.js")
 const {Affinity} = require("../models/Affinity.js")
 const {Element} = require("../models/Element.js")
+const {Users} = require('../models/Users.js')
+const {Skill} = require ('../models/Skill.js')
 
 class ModelFactory {
     static create(model, payload) {
@@ -11,6 +13,10 @@ class ModelFactory {
                 return new Affinity(payload)
             case 'element':
                 return new Element(payload)
+            case 'user':
+                return new Users(payload)
+            case 'skill_usage':
+                return new Skill(payload)
             default:
                 return 0
         }
