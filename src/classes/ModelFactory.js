b5e587d9b6c45fc1d820afbe7dbf5e076f6e83c3
stevemen20020 <david@alphadev.io>
2024-01-06 12:29:15 -0600

    Inventory Weapon Corrected

--- src/classes/ModelFactory.js
+++ src/classes/ModelFactory.js
@@ -8,6 +8,7 @@ const {Dungeon} = require ('../models/Dungeons.js')
 const {City} = require ('../models/City.js')
 const {Object} = require ('../models/Object.js')
 const {Weapon} = require('../models/Weapon.js')
+const {InventoryWeapon} = require ('../models/InventoryWeapon.js')
 
 class ModelFactory {
     static create(model, payload) {
@@ -32,6 +33,8 @@ class ModelFactory {
                 return new Object(payload)
             case 'weapon':
                 return new Weapon(payload)
+            case 'inventory_weapon':
+                return new InventoryWeapon(payload)
             default:
                 return 0
         }
