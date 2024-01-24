f18d39f858b5171764c0f7e7295ecbdd09f8d7a7
stevemen20020 <david@alphadev.io>
2024-01-24 02:30:33 -0600

    Added controller for Weapon. Need to check foreign Key in element for weapon table

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,12 @@ import characterRouter from "./routes/playable-character.router.js";
 import elementRouter from "./routes/elemenet.router.js";
 import affinityRouter from "./routes/affinity.router.js";
 import usersRouter from "./routes/users.router.js";
+import WeaponRouter from "./routes/weapon.router.js";
 
 router.use('/playable_character', characterRouter)
 router.use('/element', elementRouter)
 router.use('/affinity', affinityRouter)
 router.use('/user', usersRouter)
+router.use('/weapon', WeaponRouter)
 
 export default router
