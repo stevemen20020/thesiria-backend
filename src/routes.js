de9ba9bde783f3c59e718781f3501b86ee7da3f9
lordmarkvet <153802217+lordmarkvet@users.noreply.github.com>
2024-03-07 01:57:36 -0600

    Missions

--- src/routes.js
+++ src/routes.js
@@ -22,6 +22,7 @@ import devilFruitRouter from "./routes/devil_fruit.router.js";
 import dungeonRouter from "./routes/dungeon.router.js";
 import effectsRouter from "./routes/effects.router.js";
 import hakiRouter from "./routes/haki.router.js";
+import missionsRouter from "./routes/missions.router.js";
 
 router.use('/playable_character', characterRouter)
 router.use('/element', elementRouter)
@@ -43,5 +44,6 @@ router.use('/devil_fruit', devilFruitRouter)
 router.use('/dungeon', dungeonRouter)
 router.use('/effects', effectsRouter)
 router.use('/haki', hakiRouter)
+router.use('/missions', missionsRouter)
 
 export default router
